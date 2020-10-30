3a7c334693d9b47e2ded22d14da2c47b1d76672e
aledure <mandrew.ledure@gmail.com>
2020-10-29 19:40:29 -0500

    adding members card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
+import { MatCardModule } from '@angular/material/card';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TeamContainerComponent } from './team-container/team-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamContainerComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
