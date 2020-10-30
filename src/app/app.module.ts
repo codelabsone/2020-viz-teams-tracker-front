dae0f35d561773423000e8d4349fc67784cdcea7
Stephen Kline <Stephenkline09@gmail.com>
2020-10-29 19:29:28 -0500

    Merged with Master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TeamsListComponent } from './teams-list/teams-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamsListComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
